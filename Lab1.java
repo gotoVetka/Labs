b725a220fc9fe62cc1cdcf95ac62b78313c6fd4d
gotoVetka <noned4b@gmail.com>
2025-09-15 22:29:11 +0300

    Update Lab1.java
    Whole Lab1 in one file

--- Lab1.java
+++ Lab1.java
@@ -2,33 +2,95 @@ import java.util.*;
 public class Lab1{
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args){
+        Task tasks = new Task();
+        int userChoice;
+        while(true){
+            System.out.println("\n1 - Task 1\n2 - Task 2\n3 - Task 3\n4 - Task 4\n5 - Exit Program");
+            userChoice = scan.nextInt();
+            try{
+                switch (userChoice) {
+                    case 1:
+                        tasks.task1();
+                        break;
+                    case 2:
+                        tasks.task2();
+                        break;
+                    case 3:
+                        tasks.task3();
+                        break;
+                    case 4:
+                        tasks.task4();
+                        break;
+                    case 5:
+                        return;
+                    default:
+                        System.out.println("Unknown func");
+                        break;
+                }
+            }
+            catch(Exception ex){
+                System.out.println(ex);
+            }
+        }
+    }
+}
+public class Task{
+    Scanner task_scan = new Scanner(System.in);
+    public void task1(){
+        for(int i=1; i <= 500; i++){
+            System.out.printf("\nCase " + i + ": ");
+            if(i%5==0){
+                System.out.print("fizz");
+            }
+            if(i%7==0){
+                System.out.print("buzz");
+                continue;
+            }
+        }
+    }
+    public void task2(){
+        System.out.println("Enter String: ");
+        String string = task_scan.nextLine();
+        for(int i = string.length()-1; i>=0; --i){
+            System.out.print(string.charAt(i));
+        } 
+    }
+    public void task3(){
+        int prev = 0;
+        int cur = 1;
+        int fut;
+        System.out.println("Iterations: ");
+        int iter = task_scan.nextInt();
+        for (int i = 1; i <= iter; i++){
+            System.out.print(" " + cur);
+            fut = cur + prev;
+            prev = cur;
+            cur = fut;
+            
+        }
+    }
+    public void task4(){
         int[] numbers = new int[3];
         for(int i = 0; i < numbers.length; i++){
-            numbers[i] = scan.nextInt();
+            System.out.print("\n: " );
+            numbers[i] = task_scan.nextInt();
         }
-        printArr(numbers, "Array:");
-        int arr_min = findArrMin(numbers);  
+        System.out.println("Array: ");
+        for(int a : numbers){
+            System.out.print(a + " ");
+        }
+        System.out.println("");
+        int arr_min=0;
+        for(int cur_arr : numbers){
+            if(arr_min>cur_arr)arr_min=cur_arr;
+        }
         System.out.println("Minimun: " + arr_min);
         int max_multiplier = 0;
         arr_min = Math.abs(arr_min);
-        for(int i = 1; i < arr_min+1; i++){
+        for(int i = 1; i <= arr_min; i++){
             if(numbers[0]%i==0 && numbers[1]%i==0 && numbers[2]%i==0)max_multiplier=i;
         }
         System.out.println("Max multiplier: " + max_multiplier);
     
-    }
-    public static void printArr(int[] arr, String toPrint){
-        System.out.println(toPrint);
-        for(int a : arr){
-            System.out.print(a + " ");
-        }
-        System.out.println("");
-    }
-    public static int findArrMin(int[] arr){
-        int min = arr[0];
-        for(int cur_arr : arr){
-            if(min>cur_arr)min=cur_arr;
-        }
-        return min;
     }
 }
